deddeae3d34f3242599e7aecfa632138d0e49743
Adrien FERRER <adrien.ferrer04@gmail.com>
2018-05-09 12:13:12 +0200

    Navbar update
    added login & logout btn

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -4,21 +4,20 @@ class Navbar extends React.Component {
     render() {
         return (
         	<div>
-				<nav class="navbar navbar-expand-lg navbar-light bg-light col-xl-12">
+				<nav class="navbar navbar-expand-lg navbar-light bg-light col-12">
+					<a href="https://www.isep.fr/"><img src ="http://www.financetesetudes.com/wp-content/uploads/2013/08/logo-isep.jpg" width="6%" height="6%"/> </a>
 					<ul className="nav navbar-nav">
 						<li>
-							<Link to="/">Food Jokes</Link>
+							<a href="https://educ.isep.fr/moodle/login/index.php" className="btn btn-warning log"> Moodle </a>
 						</li>
 						<li>
-							<Link to="/special">Celebrity Jokes</Link>
+							<a href="http://weberp.isep.fr" className="btn btn-warning log"> Weberp </a>
 						</li>
 					</ul>
-
-
-
-				  <a href="https://www.isep.fr/"><img src ="http://www.financetesetudes.com/wp-content/uploads/2013/08/logo-isep.jpg" width="6%" height="6%"/> </a>
-				  <a href="https://educ.isep.fr/moodle/login/index.php"> Moodle </a> 
-				  <a href="http://weberp.isep.fr"> Weberp </a>
+					<ul className="nav navbar-nav navbar-right">
+						<li><button className="btn btn-success log">S'identifier</button></li>
+						<li><button className="btn btn-danger log">Se déconnecter </button></li>
+					</ul>
 				</nav>
 			</div>
 );}
