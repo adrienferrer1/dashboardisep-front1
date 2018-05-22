898642a8d1c49dae8ee2008d7d28c37890819e5c
AdrienFerrer <adrien.ferrer@isep.fr>
2018-05-22 11:09:21 +0200

    Tables JSON
    All tables are now filled with external data.

--- src/components/pages/planification/tasklist.js
+++ src/components/pages/planification/tasklist.js
@@ -2,44 +2,71 @@ import React, { Component } from 'react';
 
 
 class Tasklist extends Component {
+    constructor() {
+        super();
+        this.state = {
+            data: [{
+                title : "Rédiger le CdC",
+                priority: "4",
+                name: "Aymeric",
+                lastname : "De Javel"
+            },{
+                title : "Développer la page de login",
+                priority: "2",
+                name: "Alain",
+                lastname : "Rizkallah"
+            }, {
+                title : "Déployer le site web",
+                priority: "3",
+                name: "Francçois",
+                lastname : "De Verdun"
+            },]
+        }
+    }
     render() {
+        let rows = this.state.data.map(person => {
+            return <TaskRow key = {
+                person.name
+            } data = {person}
+            />
+        })
         return (
-            <div className="Tasklist" class="col-xl-12">
+            <div className="col-12">
                 <br></br>
-                <h5> Liste des tâches </h5>
-                <table class="table table-bordered">
+                <h5>Liste des tuteurs</h5>
+                <table className="col-xl-12 table">
+
                     <thead>
                     <tr>
-                        <th scope="col" class="col-xl-6">Intitulé</th>
-                        <th scope="col">Priorité /5</th>
-                        <th scope="col">Nom du réalisateur</th>
-                        <th scope="col">Prénom du réalisateur</th>
+                        <th>Intitulé</th>
+                        <th>Priorité /5</th>
+                        <th>Nom réalisateur</th>
+                        <th>Prénom réalisateur</th>
                     </tr>
                     </thead>
-                    <tbody>
-                    <tr>
-                        <th scope="row">Tâche 1</th>
-                        <td>4</td>
-                        <td>Nom 1</td>
-                        <td>Prénom 1</td>
-                    </tr>
-                    <tr>
-                        <th scope="row">Tâche 2</th>
-                        <td>1</td>
-                        <td>Nom 2</td>
-                        <td>Prénom 2</td>
-                    </tr>
-                    <tr>
-                        <th scope="row">Tâche 3</th>
-                        <td>2</td>
-                        <td>Nom 3</td>
-                        <td>Prénom 3</td>
-                    </tr>
-                    </tbody>
-                </table>
-            </div>
-        );
+                    < tbody > {
+                        rows
+                    } </tbody></table></div>);
     }
+}
+
+const TaskRow = (props) => {
+    return (
+        <tr>
+            <td>
+                { props.data.title }
+            </td>
+            <td>
+                { props.data.priority }
+            </td>
+            <td>
+                { props.data.name }
+            </td>
+            <td>
+                { props.data.lastname }
+            </td>
+        </tr>
+    );
 }
 
 export default Tasklist;
