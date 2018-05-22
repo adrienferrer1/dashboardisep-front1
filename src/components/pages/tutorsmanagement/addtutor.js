4313be9c3f0c068b2dd7585474268db54f47ba6a
AdrienFerrer <adrien.ferrer@isep.fr>
2018-05-22 14:02:05 +0200

    Finished Dynamic tables

--- src/components/pages/tutorsmanagement/addtutor.js
+++ src/components/pages/tutorsmanagement/addtutor.js
@@ -1,27 +1,44 @@
 import React, { Component } from 'react';
 
 class Addtutor extends Component {
-    render() {
-    	return (
-    		<div className="Addtutor" class="col-xl-5">
-				<br></br>
-    			<h5 class="container"> Ajouter un tuteur</h5>
-    			<br></br>
-				<form class="container">
-				  <div class="form-group">
-				    <input type="text" class="form-control" id="Nom" placeholder="Nom"/>
-				  </div>
-				  <div class="form-group">
-				    <input type="text" class="form-control" id="Prénom" placeholder="Prénom"/>
-				  </div>
-				  <div class="form-group">
-				    <input type="email" class="form-control" id="mail" placeholder="e-mail"/>
-				  </div>
-				  <button type="submit" class="btn btn-primary">Valider</button>
-				</form>
+    constructor(props) {
+        super(props);
+        this.state = {value: ''};
 
-	    	</div>
-    		);
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+    handleChange(event) {
+        this.setState({value: event.target.value});
+    }
+
+    handleSubmit(event) {
+        alert('A name was submitted: ' + this.state.value);
+        event.preventDefault();
+    }
+
+
+
+    render() {
+        return <div className="Addtutor" class="col-xl-5">
+            <br></br>
+            <h5 class="container"> Ajouter un tuteur</h5>
+            <br></br>
+            <form class="container" onSubmit={this.handleSubmit}>
+                <div class="form-group">
+                    <input type="text" class="form-control" value={this.state.value} onChange={this.handleChange}
+                           placeholder="Nom"/>
+                </div>
+                <div class="form-group">
+                    <input type="text" class="form-control" id="Prénom" placeholder="Prénom"/>
+                </div>
+                <div class="form-group">
+                    <input type="email" class="form-control" id="mail" placeholder="e-mail"/>
+                </div>
+                <button type="submit" class="btn btn-primary">Valider</button>
+            </form>
+
+        </div>;
     }
 }
 
