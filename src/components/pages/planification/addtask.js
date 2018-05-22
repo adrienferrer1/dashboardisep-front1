41cdc7af6636557327cbbfaf7f6bf47066c93bc5
AdrienFerrer <adrien.ferrer@isep.fr>
2018-05-22 15:36:15 +0200

    Addtask dynamic form
    Addtask form working properly and able to send data

--- src/components/pages/planification/addtask.js
+++ src/components/pages/planification/addtask.js
@@ -1,6 +1,39 @@
 import React, { Component } from 'react';
 
 class Addtask extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {title: '', priority:'', enddate: '', phase:'',comment:''};
+
+        this.handleTitleChange = this.handleTitleChange.bind(this);
+        this.handlePriorityChange = this.handlePriorityChange.bind(this);
+        this.handleEnddateChange = this.handleEnddateChange.bind(this);
+        this.handlePhaseChange = this.handlePhaseChange.bind(this);
+        this.handleCommentChange = this.handleCommentChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+    handleTitleChange(event) {
+        this.setState({title: event.target.value});
+    }
+    handlePriorityChange(event) {
+        this.setState({priority: event.target.value});
+    }
+    handleEnddateChange(event) {
+        this.setState({enddate: event.target.value});
+    }
+    handlePhaseChange(event) {
+        this.setState({phase: event.target.value});
+    }
+    handleCommentChange(event) {
+        this.setState({comment: event.target.value});
+    }
+
+
+    handleSubmit(event) {
+        alert(this.state.title  + this.state.priority + this.state.enddate + this.state.phase + this.state.comment );
+        event.preventDefault();
+    }
+
     render() {
     	return (
     		<div className="Addtask" class="col-xl-12">
@@ -9,31 +42,31 @@ class Addtask extends Component {
 				<form id="addtask">
 				  <div class ="row">
 					  <div class="form-group col-xl-6">
-					    <input type="text" class="form-control" id="Intitulé" placeholder="Intitulé"/>
+					    <input type="text" value={this.state.title} onChange={this.handleTitleChange} class="form-control" id="Intitulé" placeholder="Intitulé"/>
 					  </div>
 					  <div class="form-group col-xl-2">
-					    <input type="Number" class="form-control" id="Priorité" placeholder="Priorité"/>
+					    <input type="Number" value={this.state.priority} onChange={this.handlePriorityChange} class="form-control" id="Priorité" placeholder="Priorité"/>
 					  </div>
 					  <div class="form-group row">
 					    <p class="col-xl-7">date de fin théorique : </p>
-					    <input type="date" class="col-xl-5 form-control" id="date de fin théorique" placeholder="Date de fin théorique"/>
+					    <input type="date" value={this.state.enddate} onChange={this.handleEnddateChange} class="col-xl-5 form-control" id="date de fin théorique" placeholder="Date de fin théorique"/>
 					  </div>
 				  </div>
 				  <div class ="row">
 					  <div class="form-group col-xl-6">
-					  	<select class="custom-select" name="Phase" form="addtask">
+					  	<select class="custom-select" value={this.state.phase} onChange={this.handlePhaseChange} name="Phase" form="addtask">
 					  		<option value="" disabled selected>Sélectionner une phase</option>
 					  		<option>Phase 1</option>
 					  		<option>Phase 2</option>
 					  	</select>
 					  </div>
 					  <div class="col-xl-6">
-					    <textarea class="form-control" id="Commentaires" placeholder="Commentaires"/>
+					    <textarea class="form-control" value={this.state.comment} onChange={this.handleCommentChange} id="Commentaires" placeholder="Commentaires"/>
 					  </div>
 				  </div>
 
 
-				  <button type="submit" class="btn btn-primary">Valider</button>
+				  <button type="submit" class="btn btn-primary" onClick={this.handleSubmit}>Valider</button>
 				</form>
 
 	    	</div>
