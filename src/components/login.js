96db29e7f85b9df0a15abb5b01b9ae0ccd3fd223
AdrienFerrer <adrien.ferrer@isep.fr>
2018-05-26 14:33:13 +0200

    Axios
    Added Axios lib (in order to perform GET & POST) request to the API

--- src/components/login.js
+++ src/components/login.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
-
+import axios from 'axios';
+import browserhistory from 'react-router';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { Redirect } from 'react-router-dom'
 
 class Login extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {login: '', password:'', login1:'', password1:'',password2:'', name:'', lastname:''};
+        this.state = {login: '', password:'', mail:'', password1:'',password2:'', name:'', lastname:''};
         //Login Form
         this.handleLoginChange = this.handleLoginChange.bind(this);
         this.handlePasswordChange = this.handlePasswordChange.bind(this);
@@ -14,7 +16,7 @@ class Login extends React.Component {
         //Subscribe Form
         this.handleNameChange = this.handleNameChange.bind(this);
         this.handlelastNameChange = this.handlelastNameChange.bind(this);
-        this.handleLogin1Change = this.handleLogin1Change.bind(this);
+        this.handleMailChange = this.handleMailChange.bind(this);
         this.handlePassword1Change = this.handlePassword1Change.bind(this);
         this.handlePassword2Change = this.handlePassword2Change.bind(this);
         this.handleSubscribeSubmit = this.handleSubscribeSubmit.bind(this);
@@ -30,7 +32,6 @@ class Login extends React.Component {
         alert(this.state.login  + this.state.password);
         event.preventDefault();
     }
-
     //Subscribe Form Handle functions
     handleNameChange(event) {
         this.setState({name: event.target.value});
@@ -38,9 +39,8 @@ class Login extends React.Component {
     handlelastNameChange(event) {
         this.setState({lastname: event.target.value});
     }
-
-    handleLogin1Change(event) {
-        this.setState({login1: event.target.value});
+    handleMailChange(event) {
+        this.setState({mail: event.target.value});
     }
     handlePassword1Change(event) {
         this.setState({password1: event.target.value});
@@ -50,18 +50,21 @@ class Login extends React.Component {
     }
     handleSubscribeSubmit(event) {
         if (this.state.password1 != this.state.password2){
+
             alert('Les mots de passes entrés sont différents');
+            //return <Redirect to='/Tracking'/>
         }
         else{
-            alert(this.state.login1  + this.state.password1 + this.state.password2+ this.state.name+ this.state.lastname);
+            axios.post('https://back-dashboardisep.projects.jcloud.fr/users/add',{name: this.state.name, lastname: this.state.lastname, email: this.state.mail, password: this.state.password1}).then(function (response) {
+                console.log(response);
+            }).catch(function (error) {
+                console.log(error);
+            });
+            //alert(this.state.login1  + this.state.password1 + this.state.password2+ this.state.name+ this.state.lastname);
             //Suite de la requête
         }
-
-
         event.preventDefault();
     }
-
-
     render() {
         return (
             <div>
@@ -85,11 +88,11 @@ class Login extends React.Component {
                         <br></br>
                         <input type="text" value={this.state.lastname} onChange={this.handlelastNameChange} className="form-control" placeholder="Nom" name="lastname"/>
                         <br></br>
-                        <input type="text" value={this.state.login1} onChange={this.handleLogin1Change} className="form-control" placeholder="Mail" name="login"/>
+                        <input type="text" value={this.state.mail} onChange={this.handleMailChange} className="form-control" placeholder="Mail" name="mail"/>
                         <br></br>
-                        <input type="password" value={this.state.password1} onChange={this.handlePassword1Change} className="form-control" placeholder="Mot de passe" name="password"/>
+                        <input type="password" value={this.state.password1} onChange={this.handlePassword1Change} className="form-control" placeholder="Mot de passe" name="password1"/>
                         <br></br>
-                        <input type="password" value={this.state.password2} onChange={this.handlePassword2Change} className="form-control" placeholder="Mot de passe" name="password"/>
+                        <input type="password" value={this.state.password2} onChange={this.handlePassword2Change} className="form-control" placeholder="Mot de passe" name="password2"/>
                         <br></br>
                         <button className="btn btn-warning" onClick={this.handleSubscribeSubmit}>
                             S'inscrire
