cc086e0dc27e9920c47889c461ef50959aa03e29
AdrienFerrer <adrien.ferrer@isep.fr>
2018-06-05 17:40:45 +0200

    Done tasks
    Fully functional table on "tracking page", getting data from API

--- src/components/pages/tracking/done_tasks.js
+++ src/components/pages/tracking/done_tasks.js
@@ -1,30 +1,32 @@
 import React, { Component } from 'react';
+import axios from "axios/index";
 
 
 class Done_tasks extends React.Component {
     constructor() {
         super();
         this.state = {
-            data: [{
-                title : "Rédiger le CdC",
-                theoricalEndDate : "12/05/18",
-                endDate: "Aymeric",
-                duration : "10"
-            },{
-                title : "Rédiger le CdC",
-                theoricalEndDate : "12/05/18",
-                endDate: "Aymeric",
-                duration : "10"
-            }, {
-                title : "Rédiger le CdC",
-                theoricalEndDate : "12/05/18",
-                endDate: "Aymeric",
-                duration : "10"
-            },]
+            done_tasks:[],
         }
     }
+
+    componentDidMount(){
+        let done_tasks = [];
+        axios.defaults.headers.common['Authorization'] = sessionStorage.getItem('token');
+        axios.get('https://back-dashboardisep.projects.jcloud.fr/tasks/all')
+            .then(response => {
+                for (var i = 0; i < response.data.length; i++) {
+                    if (response.data[i].done==false) {
+                        done_tasks.push(response.data[i]);
+                    }
+                }
+                this.setState({done_tasks});
+                console.log(this.state.done_tasks);
+            });
+    }
+
     render() {
-        let rows = this.state.data.map(person => {
+        let rows = this.state.done_tasks.map(person => {
             return <TaskRow key = {
                 person.name
             } data = {person}
@@ -39,8 +41,8 @@ class Done_tasks extends React.Component {
                     <thead>
                     <tr>
                         <th>Intitulé</th>
+                        <th>Date de début</th>
                         <th>Date de fin théorique</th>
-                        <th>Date de fin</th>
                         <th>Durée</th>
                     </tr>
                     </thead>
@@ -54,20 +56,42 @@ const TaskRow = (props) => {
     return (
         <tr>
             <td>
-                { props.data.title }
+                { props.data.name }
             </td>
             <td>
-                { props.data.theoricalEndDate }
+                { timeConverter(props.data.start_date) }
             </td>
             <td>
-                { props.data.endDate }
+                { timeConverter(props.data.end_date) }
             </td>
             <td>
-                { props.data.duration }
+                { hourConverter(Duration(props.data.end_date,props.data.start_date)) }j
             </td>
         </tr>
     );
 }
 
+function timeConverter(UNIX_timestamp){
+        var a = new Date(UNIX_timestamp * 1000);
+        var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+        var year = a.getFullYear();
+        var month = months[a.getMonth()];
+        var date = a.getDate();
+        var time = date + ' ' + month + ' ' + year ;
+    return time;
+}
 
+function hourConverter(time) {
+    var hours;
+    hours = time / (3600*24);
+    return hours;
+}
+
+
+function Duration(date1,date2){
+    var time;
+    time = Math.abs(date2 - date1);
+    return time;
+
+}
 export default Done_tasks;
